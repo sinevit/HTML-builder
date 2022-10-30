4a23159c54469d7f8f1c715d92c46a2f5bb2d596
Kris <Krismail>
2022-10-30 18:18:03 +0300

    feat: add 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+// const fs = require('fs');
+const path = require('path');
+const fs = require('fs/promises');
+const src =  path.join(__dirname, '/secret-folder');
+
+
+(async function checkFiles(src){
+try {
+    const files = await fs.readdir( src,{withFileTypes: true});
+    for (const file of files){
+        if (file.isFile()) {
+            let fileName = path.parse(path.join(src, file.name)).name;
+            let fileExt = path.parse(path.join(src, file.name)).ext.slice(1);
+            let fileSize = (await fs.stat(path.join(src, file.name))).size/1024;
+
+            console.log(`${fileName} - ${fileExt} - ${Math.ceil(fileSize)}kb`);
+        }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+})(src);
